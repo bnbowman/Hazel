d078d714bb154a391b6467908a439c51a64b9789
Brett Bowman <bnbowman@Bretts-MacBook-Pro.local>
2020-10-03 10:06:34 -0700

    Move application code to Sandbox

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -10,7 +10,20 @@
 #include "Hazel/Core/Layer.h"
 #include "Hazel/Core/Log.h"
 #include "Hazel/Core/MouseCodes.h"
+
 #include "Hazel/ImGui/ImGuiLayer.h"
+
+// === Render API ===
+#include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/RenderCommand.h"
+
+#include "Hazel/Renderer/IndexBuffer.h"
+#include "Hazel/Renderer/VertexBuffer.h"
+#include "Hazel/Renderer/VertexArray.h"
+#include "Hazel/Renderer/Shader.h"
+
+#include "Hazel/Renderer/OrthographicCamera.h"
+// === Render API ===
 
 // === Entry Point ===
 #include "Hazel/EntryPoint.h"
